336d50fdc40ab8505dc1d75098fbbae654e6dd77
Harley McDowell <hmcdowell95@gmail.com>
2020-05-27 02:37:42 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -15,7 +15,7 @@ class StudentsController < ApplicationController
     else
       @student.active = false 
     end
-    redirect_to "/students/:id"
+    redirect_to "/students/#{@student.id}"
   end
   
   private
